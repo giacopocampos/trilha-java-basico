fc0c836457cbc5a945af87c9e927c16bad20c8bd
Giácopo Campos <122495566+giacopocampos@users.noreply.github.com>
2024-06-02 13:53:49 -0300

    sistematv

---
+++ tipos-variaveis/src/SistemaSmartTV.java
@@ -0,0 +1,20 @@
+public class SistemaSmartTV {
+ 
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+
+        ligada = true;
+
+    }
+
+    public void desligar(){
+
+        ligada = false;
+
+    }
+
+
+}
---
+++ tipos-variaveis/src/Usuario.java
@@ -0,0 +1,19 @@
+public class Usuario {
+    
+    public static void main(String[] args) {
+        
+        SistemaSmartTV smartTV = new SistemaSmartTV();
+
+        System.out.println("TV Ligada? " + smartTV.ligada);
+        System.out.println("Canal Atual: " + smartTV.canal);
+        System.out.println("Volume Atual: " + smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("TV Ligada? " + smartTV.ligada);
+
+        smartTV.desligar();
+        System.out.println("TV Ligada? " + smartTV.ligada);
+
+
+    }
+}
